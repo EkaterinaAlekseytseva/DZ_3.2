7de36b68b9dac357b144607639fa44b424423f59
Ekaterina <alexeitseva.katya@yandex.ru>
2022-10-23 19:08:07 +0700

    Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное (целое) число и на выходе (в териминале) показывает вторую цифру этого числа. Через строку решать нельзя.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine()!);
+
+int GetDigit(int number)
+{
+    int firstDigit = number / 10;
+    int secontDigit = number / 10;
+    int GetDigit = secontDigit % 10;
+    return GetDigit;
+}
+
+Console.WriteLine(GetDigit(number));
